cf1b240159f2f343f00d9eebaedba54719023999
you-u-u <a6cd1f.g@gmail.com>
2024-08-23 14:05:45 +0900

    paiza

--- paiza_d/sosu.rb
+++ paiza_d/sosu.rb
@@ -1,6 +1,23 @@
-require "prime"
+n = gets.to_i
+sosu = []
 
-a,b=gets.split(" ").map(&:to_i)
+(1..n).each do |i|
+  next if i == 1
 
-count=[]
-(a..b).each do |i|
+  if i == 2
+    sosu.push(i)
+    next
+  end
+  is_prime = true
+
+  sosu.each do |num|
+    if i % num == 0
+      is_prime = false
+      break
+    end
+  end
+
+  sosu.push(i) if is_prime
+end
+puts sosu.size
+
