e7b808ca4e61456980a41f4118b764f42ed4503f
you-u-u <a6cd1f.g@gmail.com>
2024-09-06 07:51:35 +0900

    paiza

--- paiza_b/B138.rb
+++ paiza_b/B138.rb
@@ -1,7 +1,24 @@
-n, w = gets.split.map(&:to_i)
-picture = Array.new(n,Array.new(w))
+h, w = gets.split.map(&:to_i)
+art = []
+doughnuts = 0
+h.times do
+  art << gets.chomp
+end
+p art
 
-n.times do
-  picture << gets.chomp
-  if array == "###"
-    
+(0...(h-2)).each do |i|
+  (0...(w-2)).each do |j|
+    p art[i][j..(j+2)]
+    p "next"
+    if art[i][j..(j+2)] == "###" &&
+      art[i+1][j..(j+2)] == "#.#" &&
+      art[i+2][j..(j+2)] == "###"
+      doughnuts += 1
+    end
+  end
+end
+
+puts doughnuts
+
+
+
